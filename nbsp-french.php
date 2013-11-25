c1345cf3881e3d912a0efea865acc8312c74513c
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-25 14:20:00 +0000

    v1.0
    git-svn-id: https://plugins.svn.wordpress.org/nbsp-french/trunk@810134 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nbsp-french.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: nbsp French
+Plugin URI: http://surniaulula.com/wordpress-plugins/nbsp-french/
+Author: Jean-Sebastien Morisset
+Author URI: http://surniaulula.com/
+Description: Adds a non-breaking space between words and exclamation, question/interrogation marks. 
+Version: 1.0
+
+Copyright 2012 - Jean-Sebastien Morisset - http://surniaulula.com/
+
+This script is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This script is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details at
+http://www.gnu.org/licenses/.
+*/
+
+add_filter( 'the_content', 'nbsp_french' );
+add_filter( 'the_excerpt', 'nbsp_french' );
+add_filter( 'comment_text', 'nbsp_french' );
+
+function nbsp_french( $content ) {
+	$in_code = '';
+	$new_content = '';
+	// split content to exclude comments and javascript sections
+	foreach ( preg_split( '/((\r?\n)|(\r\n?))/', $content) as $line) {
+		if ( preg_match( '/<(!--|script|pre)/i', $line ) ) $in_code = 1;
+		if ( preg_match( '/(--|\/script|\/pre)>/i', $line ) ) $in_code = 0;
+		if ( empty( $in_code ) ) {
+			$pattern = array( '/(\w) (\!|\?)/i' ); ksort($pattern);
+			$replace = array( '$1&nbsp;$2' ); ksort($replace);
+			$line = preg_replace( $pattern, $replace, $line);
+		}
+		$new_content .= $line."\n";
+	} 
+	return $new_content;
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== nbsp French ===
+Contributors: jsmoriss
+Donate link: http://surniaulula.com/extend/plugins/contribution-form/
+Tags: nbsp, french, exclamation, question, interrogation, line-break
+License: GPLv2 or later
+Requires at least: 3.0
+Tested up to: 3.7.1
+Stable tag: 1.0
+
+Adds a non-breaking space between words and exclamation, question/interrogation marks. 
+
+== Description ==
+
+Adds a non-breaking space between words and exclamation, question/interrogation marks to avoid inappropriate line-breaks in French.
+
+== Installation ==
+
+*Using the WordPress Dashboard*
+
+1. Login to your weblog
+1. Go to Plugins
+1. Select Add New
+1. Search for *nbsp french*
+1. Select Install
+1. Select Install Now
+1. Select Activate Plugin
+
+*Manual*
+
+1. Download and unzip the plugin
+1. Upload the entire span-file-and-vars/ folder to the /wp-content/plugins/ directory
+1. Activate the plugin through the Plugins menu in WordPress
+
+== Frequently Asked Questions ==
+
+== Changelog ==
+
+= Version 1.0 =
+
+* Initial release.
+
